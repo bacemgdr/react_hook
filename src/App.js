60bf3ee3c09f654e188a6bf717a704c9525e3d1d
bacemgdr <bacem.ghdiri@gmail.com>
2023-11-11 12:35:00 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
-
+import MovieslistView from "./components/MovieList";
+import FilterMovie from "./components/FilterMovie";
+import { DefautlMovies } from "./components/Movie";
 function App() {
+  /* movieslist state take by default the list from Movies file */
+  const [movies, setMovies] = useState(DefautlMovies)
+  /* 2 state for filtring by title/rating */
+  const [filterbytitle, setFilterbytitle] = useState("")
+  const [filterbyrating, setFilterbyrating] = useState(0)
+
+  /* function to add new movie to the movies list */
+  const addMovie = (newMovie) => {
+    setMovies([newMovie, ...movies]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* first row to display the filtring components */}
+      <div class="row">i
+        <FilterMovie
+          setFilterbytitle={setFilterbytitle}
+          setFilterbyrating={setFilterbyrating}
+          rating={filterbyrating}
+          addMovie={addMovie}
+        />
+      </div>
+      <div class="row">
+        {/* second row to display the movies list */}
+        <MovieslistView
+          moviesList={movies}
+          titlefilter={filterbytitle}
+          ratingfilter={filterbyrating}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/MovieCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Card from 'react-bootstrap/Card';
+import Stack from 'react-bootstrap/Stack';
+import Rating from '@mui/material/Rating';
+import Badge from 'react-bootstrap/Badge';
+
+function MovieCardView(props) {
+  const movie = props.movie
+  return (
+    /* a card to display echt movie variable */
+    <Card bg="dark" className="movie-card">
+      <img src={movie.posterURL} alt="" />
+      <Card.Body>
+        <Card.Title className="text-success" >{movie.title}</Card.Title>
+        <Card.Text className="description-color" >
+      
+              {movie.description}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <Stack direction="horizontal" gap={3}>
+        
+          <div className="p-2 ms-auto">
+          <h6>Rating</h6>
+            <Badge bg="secondary">
+              <Rating name="half-rating-read"  rating value= {movie.rating} precision={0.5}  />
+            </Badge>
+          </div>
+        </Stack>
+      </Card.Footer>
+    </Card>
+  );
+}
+
+export default MovieCardView;
